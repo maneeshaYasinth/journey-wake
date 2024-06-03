6f1413c9cd3b73820f1caf7e0333cdf7a51afd42
Udev2002 <udayanga.dev.lk@gmail.com>
2024-06-03 13:02:31 -0700

    add select location ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import StartPage from './components/StartPage/StartPage'
-import MainMap from './components/MainMap/MainMap'
-import TurnOn from './components/TurneOnLocation/TurnOn'
+import React from "react";
+import StartPage from "./components/StartPage/StartPage";
+import MainMap from "./components/MainMap/MainMap";
+import TurnOn from "./components/TurneOnLocation/TurnOn";
+import MapNew from "./components/MapNew/MapNew";
+
 
 const App = () => {
   return (
     <div>
-      <StartPage/>
+      <StartPage />
       {/* <TurnOn/> */}
-      <MainMap/>
+      {/* <MainMap/> */}
+      <MapNew/>
+   
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/MapNew/MapNew.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { MdMyLocation } from "react-icons/md";
+import { FaLocationArrow } from "react-icons/fa";
+import Button from "../Button/Button";
+
+const MapNew = () => {
+  return (
+    <div className=" flex flex-col">
+      <section className=" flex flex-col justify-center text-center items-center md:px-20">
+        {/* map display */}
+        <div>
+          <h1>map displaye</h1>
+        </div>
+        {/* set location */}
+        <div className=" font-poppins bg-white p-4 px-5 rounded-2xl shadow-xl shadow-blue-900 md:p-20 ">
+          <div className="">
+            <div className=" font-semibold text-xl md:text-2xl ">
+              <h1>Starting point</h1>
+            </div>
+            <div className=" flex  justify-between gap-10 mt-5">
+              <div className=" text-4xl ">
+                <MdMyLocation />
+              </div>
+              <div>
+                <input
+                  className=" w-[300px] h-[50px] rounded-[10px] p-[10px] outline-none border-[2px] border-gray-500"
+                  type="text"
+                  placeholder="Your location"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-5">
+            <div>
+              <h1 className="font-semibold text-xl md:text-2xl">Finish Point</h1>
+            </div>
+            <div className=" flex  justify-between mt-5">
+              <div className=" text-3xl">
+                <FaLocationArrow />
+              </div>
+              <div>
+                <input
+                  className=" w-[300px] h-[50px] rounded-[10px] p-[10px] outline-none border-[2px] border-gray-500"
+                  type="text"
+                  placeholder="Destination"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* button */}
+        <div className=" mt-10  text-2xl md:text-3xl font-poppins ">
+          <Button
+            text={"Set Your Destination"}
+            onClick={() => {
+              alert("Destination set succussfully");
+            }}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MapNew;
